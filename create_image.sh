ed8c927d1b83bbfa30dc1228df2de62341dcb751
Murali Krishnan <murali.mani@gmail.com>
2026-06-18 07:19:47 +0100

    Update create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -1,4 +1,3 @@
-```
 #!/bin/bash
 
 rm -rf 2026-04-21-raspios-trixie-arm64-full.img
@@ -6,5 +5,3 @@ rm -rf 2026-04-21-raspios-trixie-arm64-full.img.xz
 wget https://downloads.raspberrypi.org//raspios_full_arm64/images/raspios_full_arm64-2026-04-21/2026-04-21-raspios-trixie-arm64-full.img.xz
 echo -e "perchpi" | sudo -S sdm --customize --plugin @perchpi.plugins --extend --xmb 2048 2026-04-21-raspios-trixie-arm64-full.img
 xz -k 2026-04-21-raspios-trixie-arm64-full.img
-
-```
